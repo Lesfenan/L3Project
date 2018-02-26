0457db927ca8d3bab643b12729287eb8d8e2d2ae
DR4KEN <pogoman28@gmail.com>
2018-02-26 10:34:21 +0100

    Création des classes Rapport et Application

--- projetL3/src/Rapport.java
+++ projetL3/src/Rapport.java
@@ -2,5 +2,21 @@
 public class Rapport {
 	private String titre;
 	private String description;
-	private String ID;
+	private int ID;
+	
+	public Rapport (int newID, String nomJalon ){
+		ID = newID;
+		titre = "Rapport "+ nomJalon;
+	}
+	 /*
+	public void setTitre (String newTitre){
+		titre = newTitre;
+	}
+	*/
+	
+	public void setDescritpion (String newDescritpion){
+		description = newDescritpion;
+	}
+	
+	
 }
