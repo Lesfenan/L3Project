2a700bef1feaf20b2885a97975cdb58a5608ba4a
marwanoriginals <marwanoriginals@192.168.194.162>
2018-03-13 14:18:20 +0100

    test

--- ProjetL3/src/Authentification.java
+++ ProjetL3/src/Authentification.java
@@ -5,6 +5,9 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 
 public class Authentification extends JDialog {
 
@@ -13,6 +16,8 @@ public class Authentification extends JDialog {
 	 */
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField m_textField_ID;
+	private JPasswordField m_JPassword_Password;
 
 	/**
 	 * Launch the application.
@@ -31,11 +36,29 @@ public class Authentification extends JDialog {
 	 * Create the dialog.
 	 */
 	public Authentification() {
+		setTitle("Connexion");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel m_Label_ID = new JLabel("Identifiant");
+		m_Label_ID.setBounds(40, 61, 103, 16);
+		contentPanel.add(m_Label_ID);
+		
+		m_textField_ID = new JTextField();
+		m_textField_ID.setBounds(187, 56, 165, 26);
+		contentPanel.add(m_textField_ID);
+		m_textField_ID.setColumns(10);
+		
+		JLabel m_label_Password = new JLabel("Mot de passe");
+		m_label_Password.setBounds(40, 137, 83, 16);
+		contentPanel.add(m_label_Password);
+		
+		m_JPassword_Password = new JPasswordField();
+		m_JPassword_Password.setBounds(187, 132, 165, 26);
+		contentPanel.add(m_JPassword_Password);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -53,5 +76,4 @@ public class Authentification extends JDialog {
 			}
 		}
 	}
-
 }
